6515681e52f2d84cebb7c666d7c292280ed6bfe8
aimeele <leaimee62@gmail.com>
2024-12-20 16:31:19 +1100

    update src

--- MintClassics_InventoryAnalysis.sql
+++ MintClassics_InventoryAnalysis.sql
@@ -1,62 +1,57 @@
--- 1. Where are items stored and how many products in each warehouse?
+-- 1. Where are items stored and how many items in tocks per warehouse?
 
 SELECT 
-    p.warehouseCode, 
-    w.warehouseName, 
-    COUNT(DISTINCT p.productLine) AS productLineQty, 
-    SUM(p.quantityInStock) AS itemQty
+    p.warehouseCode,
+    w.warehouseName,
+    COUNT(DISTINCT p.productLine) AS productLineQty,
+    SUM(p.quantityInStock) AS numberInStocks,
+    ROUND(SUM(p.quantityInStock) / (SELECT 
+                    SUM(quantityInStock)
+                FROM
+                    products) * 100,
+            2) percent
 FROM
     products p
-JOIN
+        JOIN
     warehouses w ON w.warehouseCode = p.warehouseCode
-GROUP BY 
-    warehouseCode, warehouseName;
+GROUP BY warehouseCode , warehouseName;
 
--- 2. Total orders placed in each warehouse
+-- 2. Product lines distribution by warehouses
+
+SELECT DISTINCT
+    w.warehouseCode, w.warehouseName, productLine
+FROM
+    products p
+        JOIN
+    warehouses w ON w.warehouseCode = p.warehouseCode
+ORDER BY w.warehouseCode;
+
+-- 3. Items Sold per warehouse
 
 SELECT 
-    w.warehouseName, 
-    SUM(quantityOrdered) AS ProductSold
+    w.warehouseName, SUM(quantityOrdered) AS itemsSold
 FROM
     orderdetails o
-JOIN
+        JOIN
     products p ON o.productCode = p.productCode
-JOIN
+        JOIN
     warehouses w ON p.warehouseCode = w.warehouseCode
-GROUP BY 
-    w.warehouseName;
+GROUP BY w.warehouseName;
 
--- 3. Product stock and order summary for each product line per warehouse
+-- 4. Items in stock and quantity sold group by product and warehouse code
 
 SELECT 
-    p.warehouseCode, 
-    p.productLine, 
-    p.productCode, 
-    SUM(p.quantityInStock) AS InStocks, 
+    p.warehouseCode,
+    p.productLine,
+    p.productCode,
+    SUM(p.quantityInStock) AS InStocks,
     SUM(o.quantityOrdered) AS qtyordered
 FROM
     products p
-JOIN
+        JOIN
     orderdetails o ON o.productCode = p.productCode
-GROUP BY 
-    p.warehouseCode, p.productCode, p.productLine
-ORDER BY 
-    qtyordered;
-
--- 4. Product sold quantity by product code and inventory count
-
-SELECT 
-    o.productCode, 
-    p.quantityinStock, 
-    SUM(quantityOrdered) AS ProductSold
-FROM
-    orderdetails o
-JOIN
-    products p ON o.productCode = p.productCode
-GROUP BY 
-    o.productCode, p.quantityinStock
-ORDER BY 
-    ProductSold DESC;
+GROUP BY p.warehouseCode , p.productCode , p.productLine
+ORDER BY qtyordered;
 
 -- 5. Quantity sold monthly and yearly
 
@@ -81,146 +76,125 @@ GROUP BY
 ORDER BY 
     yy, mm;
 
--- 6. Average product quantities sold monthly and yearly
-
-WITH combined_tbl AS (
-    SELECT 
-        MONTH(orderDate) AS mm, 
-        YEAR(orderDate) AS yy, 
-        w.warehouseName, 
-        od.productCode, 
-        SUM(od.quantityOrdered) AS totalqtyOrders
-    FROM orders o
-    JOIN orderdetails od ON o.orderNumber = od.orderNumber 
-    JOIN products p ON p.productCode = od.productCode
-    JOIN warehouses w ON w.warehouseCode = p.warehouseCode
-    GROUP BY 
-        MONTH(orderDate), YEAR(orderDate), od.productCode, w.warehouseName
-),
-tbl1 AS (
-    SELECT mm, warehouseName, productCode, AVG(totalqtyOrders) AS avgQty
-    FROM combined_tbl
-    GROUP BY 
-        productCode, mm, warehouseName
-    ORDER BY 
-        mm
-)
-SELECT * FROM tbl1;
-
--- 7. Number of distinct product lines
-
-SELECT DISTINCT
-    productLine
-FROM
-    products;
-
--- 8. Stock quantity and order quantity per warehouse and product line
+-- 6. The stock level and quantiy sold per warehouse
 
 SELECT 
-    warehouseCode, 
-    productLine, 
-    SUM(quantityInStock) AS InStocks, 
-    SUM(quantityOrdered) AS qtyordered, 
-    SUM(quantityOrdered) / SUM(quantityInStock) AS ratio
-FROM
-    products p
-JOIN
-    orderdetails o ON o.productCode = p.productCode
-GROUP BY 
-    warehouseCode, productLine
-ORDER BY 
-    qtyordered DESC;
-
--- 9. Number of products per warehouse and product line
-
-SELECT 
-    warehouseCode, 
-    productLine, 
-    p.productCode, 
-    SUM(quantityInStock) AS InStocks, 
+    w.warehouseCode,
+    w.warehouseName,
+    SUM(quantityInStock) AS InStocks,
     SUM(quantityOrdered) AS qtyordered
 FROM
     products p
-JOIN
+        JOIN
     orderdetails o ON o.productCode = p.productCode
-GROUP BY 
-    warehouseCode, productLine, p.productCode
-ORDER BY 
-    qtyordered DESC;
-
--- 10. Warehouse stock and order quantity summary
-
-SELECT 
-    w.warehouseCode, 
-    w.warehouseName, 
-    SUM(quantityInStock) AS InStocks, 
-    SUM(quantityOrdered) AS qtyordered
-FROM
-    products p
-JOIN
-    orderdetails o ON o.productCode = p.productCode
-JOIN
+        JOIN
     warehouses w ON w.warehouseCode = p.warehouseCode
-GROUP BY 
-    w.warehouseCode, w.warehouseName;
+GROUP BY w.warehouseCode , w.warehouseName;
 
--- 11. Stock and order quantity for each product line across warehouses
+-- 7. Total Revenue of each items order by highest revenue
 
 SELECT 
-    p.productCode, 
-    p.productLine, 
-    SUM(quantityInStock) AS instocks, 
-    SUM(quantityOrdered) AS ordered
+    p.productLine, tem.productCode, SUM(totalPrice) totalRevenue
 FROM
-    products p
-JOIN
-    orderdetails o ON p.productcode = o.productCode
-GROUP BY 
-    p.productCode, p.productLine
-ORDER BY 
-    ordered;
-
--- 12. Warehouse stock and order percentages
-
-WITH cte AS (
-    SELECT 
-        p.warehouseCode, 
-        p.productLine, 
-        p.productCode, 
-        SUM(p.quantityInStock) AS InStocks, 
-        SUM(o.quantityOrdered) AS qtyordered
-    FROM products p
-    JOIN orderdetails o ON o.productCode = p.productCode
-    GROUP BY 
-        p.warehouseCode, p.productCode, p.productLine
-),
-cte1 AS (
-    SELECT *,
-        SUM(InStocks) OVER (PARTITION BY warehouseCode) AS warehouseStocks,
-        ROUND(InStocks / SUM(InStocks) OVER (PARTITION BY warehouseCode) * 100, 2) AS percent
-    FROM cte
-)
+    (SELECT 
+        productCode, quantityOrdered * priceEach totalPrice
+    FROM
+        orderdetails) tem
+JOIN products p 
+ON p.productCode = tem.productCode
+GROUP BY tem.productCode , p.productLine
+ORDER BY totalRevenue DESC;
+    
+-- 8. Total Revenue of each items order by lowest revenue
+    
 SELECT 
-    warehouseCode, 
-    AVG(qtyordered) AS avgQtyOrdered, 
-    AVG(percent) AS avgPercent
-FROM cte1
-GROUP BY 
-    warehouseCode;
-
--- 13. Warehouse capacity and stock levels
-
-SELECT 
-    w.warehouseCode, 
-    w.warehouseName, 
-    w.warehousepctCap, 
-    SUM(quantityInStock) AS InStocks, 
-    SUM(quantityOrdered) AS qtyordered
+    p.productLine, tem.productCode, SUM(totalPrice) totalRevenue
 FROM
-    products p
-JOIN
-    orderdetails o ON o.productCode = p.productCode
-JOIN
+    (SELECT 
+        productCode, quantityOrdered * priceEach totalPrice
+    FROM
+        orderdetails) tem
+JOIN products p 
+ON p.productCode = tem.productCode
+GROUP BY tem.productCode , p.productLine
+ORDER BY totalRevenue ASC;
+    
+-- 9. Revenue distribution by each product line
+SELECT 
+    p.productLine,
+    SUM(totalPrice) totalRevenue,
+    ROUND(SUM(totalPrice) / 
+                    (SELECT SUM(quantityOrdered * priceEach) totalPrice FROM orderdetails) * 100, 2) percent
+FROM
+    (SELECT 
+        productCode, quantityOrdered * priceEach totalPrice
+    FROM
+        orderdetails) tem
+JOIN products p 
+ON p.productCode = tem.productCode
+GROUP BY p.productLine
+ORDER BY totalRevenue ASC;
+    
+-- 10. Number of distinct orders
+SELECT 
+    COUNT(DISTINCT o.orderNumber) distinctOrders
+FROM
+    orderdetails od
+JOIN orders o 
+ON o.orderNumber = od.orderNumber
+WHERE
+    status = 'shipped';
+
+-- 11. Shipping duration by each orders
+SELECT 
+    o.orderNumber,
+    DATEDIFF(shippedDate, orderDate) duration,
+    comments
+FROM
+    orderdetails od
+JOIN orders o 
+ON o.orderNumber = od.orderNumber
+WHERE
+    status = 'shipped'
+GROUP BY o.orderNumber , DATEDIFF(shippedDate, orderDate)
+ORDER BY duration DESC;
+
+-- 12. Number of distinct orders after removing order that are over 10 days
+SELECT 
+    COUNT(DISTINCT orderNumber) distinctOrders
+FROM
+    (SELECT 
+        o.orderNumber,
+            DATEDIFF(shippedDate, orderDate) duration,
+            comments
+    FROM orderdetails od
+JOIN orders o 
+ON o.orderNumber = od.orderNumber
+WHERE status = 'shipped'
+    AND DATEDIFF(shippedDate, orderDate) < 10
+GROUP BY o.orderNumber , DATEDIFF(shippedDate, orderDate)) temp;
+
+-- 13. The common duration of shipping date per productLine for warehosue c and d
+
+SELECT 
+    p.productLine,
+    DATEDIFF(shippedDate, orderDate) day,
+    SUM(quantityOrdered) totalQuantity
+FROM
+    orders o
+        JOIN
+    orderdetails od ON od.orderNumber = o.orderNumber
+        JOIN
+    products p ON p.productCode = od.productCode
+        JOIN
     warehouses w ON w.warehouseCode = p.warehouseCode
-GROUP BY 
-    w.warehouseCode, w.warehouseName, w.warehousePctCap;
+WHERE
+    status <> 'shipped'
+        AND w.warehouseCode IN ('c' , 'd')
+        AND DATEDIFF(shippedDate, orderDate) < 10
+GROUP BY p.productLine , DATEDIFF(shippedDate, orderDate)
+ORDER BY productLine , DATEDIFF(shippedDate, orderDate) DESC;
+
+
+
+
